3ab634293ff195b57451e8bdc4c5432863053394
gbitd <gbitdinger@proton.me>
2026-01-12 17:04:56 -0300

    Cria api routes

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DeviceController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Rotas de Devices
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/devices', [DeviceController::class, 'index']);
+    Route::post('/devices', [DeviceController::class, 'store']);
+    Route::put('/devices/{id}', [DeviceController::class, 'update']);
+    Route::delete('/devices/{id}', [DeviceController::class, 'destroy']);
+    Route::patch('/devices/{id}/use', [DeviceController::class, 'toggleUse']);
+
+});
