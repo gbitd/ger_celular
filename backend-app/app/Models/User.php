c644d83aa05a4ceb9a24461324f3b6a8cc25b8be
gbitd <gbitdinger@proton.me>
2026-01-12 15:46:22 -0300

    Corrige Model User

--- backend-app/app/Models/User.php
+++ backend-app/app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use Notifiable, HasApiTokens;
+    use Notifiable, HasFactory, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
