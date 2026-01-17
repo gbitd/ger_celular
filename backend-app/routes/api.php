f41035684ed6894e6ecd39de80b81967e5f25639
gbitd <gbitdinger@proton.me>
2026-01-17 16:16:00 -0300

    Cria rota de logout

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -18,6 +18,8 @@ Route::get('/user', function (Request $request) {
 
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
     /*
     // Criar novo token
     Route::post('/tokens/create', function (Request $request) {
